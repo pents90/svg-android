0dbc084e2a3fea8b15f26fe54134eaa4cca80ea4
Josef Pavlik <josef@pavlik.it>
2012-12-17 15:12:00 +0100

    path command ARCTO implemented (function drawarc)

--- svgandroid/src/com/larvalabs/svgandroid/SVGParser.java
+++ svgandroid/src/com/larvalabs/svgandroid/SVGParser.java
@@ -610,10 +610,14 @@ public class SVGParser {
                     float rx = ph.nextFloat();
                     float ry = ph.nextFloat();
                     float theta = ph.nextFloat();
-                    int largeArc = (int) ph.nextFloat();
-                    int sweepArc = (int) ph.nextFloat();
+                    int largeArc = (int) ph.nextFlag();
+                    int sweepArc = (int) ph.nextFlag();
                     float x = ph.nextFloat();
                     float y = ph.nextFloat();
+                    if ( Character.isLowerCase(cmd) ) {
+                        x += lastX;
+                        y += lastY;
+                    }   
                     drawArc(p, lastX, lastY, x, y, rx, ry, theta, largeArc, sweepArc);
                     lastX = x;
                     lastY = y;
@@ -629,8 +633,75 @@ public class SVGParser {
         return p;
     }
 
+    private static float angle(float x1, float y1, float x2, float y2) {
+    	
+    	return (float) Math.toDegrees(Math.atan2(x1,y1)-Math.atan2(x2, y2))%360;
+    }
+    private static final RectF arcRectf= new RectF();
+    private static final Matrix arcMatrix=new Matrix();
+    private static final Path arcPath=new Path();
+    
     private static void drawArc(Path p, float lastX, float lastY, float x, float y, float rx, float ry, float theta, int largeArc, int sweepArc) {
-        // todo - not implemented yet, may be very hard to do using Android drawing facilities.
+//    	Log.d("drawArc","from ("+lastX+","+lastY+") to ("+x+","+y+") r=("+rx+","+ry+") theta="+theta+" flags="+largeArc+","+sweepArc);
+ 
+// http://www.w3.org/TR/SVG/implnote.html#ArcImplementationNotes
+    	
+    	if (rx==0 || ry==0) {
+   		   p.lineTo(x, y);
+   		   return;
+    	}
+    	
+    	if (x==lastX && y==lastY) return; // nothing to draw
+
+    	rx=Math.abs(rx);
+    	ry=Math.abs(ry);
+    		
+    		
+    	final float thrad=theta*(float)Math.PI/180;
+    	final float st=(float) Math.sin(thrad);
+    	final float ct=(float) Math.cos(thrad);
+    	
+    	final float xc=(lastX-x)/2;
+    	final float yc=(lastY-y)/2;
+    	final float x1t=ct*xc+st*yc;
+    	final float y1t=-st*xc+ct*yc;
+    	
+    	final float x1ts=x1t*x1t;
+    	final float y1ts=y1t*y1t;
+        float rxs=rx*rx;
+    	float rys=ry*ry;
+
+    	float lambda=(x1ts/rxs+y1ts/rys)*1.001f; // add 0.1% to be sure that no out of range occurs due to limited precision
+    	if (lambda>1) {
+    		float lambdasr=(float)Math.sqrt(lambda);
+    		rx*=lambdasr;
+    		ry*=lambdasr;
+    		rxs=rx*rx;
+    		rys=ry*ry;
+    	}
+    		   	
+    	final float R=(float)Math.sqrt((rxs*rys-rxs*y1ts-rys*x1ts)/(rxs*y1ts+rys*x1ts)) * ((largeArc==sweepArc)?-1:1);
+    	final float cxt=R*rx*y1t/ry;
+    	final float cyt=-R*ry*x1t/rx;
+    	final float cx=ct*cxt-st*cyt+(lastX+x)/2;
+    	final float cy=st*cxt+ct*cyt+(lastY+y)/2;
+    	
+    	final float th1=angle(1, 0, (x1t-cxt)/rx, (y1t-cyt)/ry);
+    	float dth=angle((x1t-cxt)/rx, (y1t-cyt)/ry, (-x1t-cxt)/rx, (-y1t-cyt)/ry);
+    	
+    	if       (sweepArc==0 && dth>0) dth-=360;
+    	else if (sweepArc!=0 && dth<0) dth+=360;
+    	
+// draw
+        arcMatrix.reset();
+        arcMatrix.postRotate(theta);
+        arcMatrix.postTranslate(cx, cy);
+
+    	arcRectf.set(-rx,-ry,rx,ry);
+        arcPath.rewind();
+    	arcPath.addArc(arcRectf, th1, dth);
+    	arcPath.transform(arcMatrix);
+    	p.addPath(arcPath);
     }
 
     private static NumberParse getNumberParseAttr(String name, Attributes attributes) {
